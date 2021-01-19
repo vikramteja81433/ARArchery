8b96a417e6be0558ac19e836a7712555421f1cd5
LAPTOP-RHR7Q8PG\Vikram teja <vikramteja81433@gmail.com>
2021-01-19 22:07:47 +0530

    latest

---
+++ MyFirstAgent/Assets/Scripts/PlayerMovementTowardsGoal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class PlayerMovementTowardsGoal : Agent
+{
+    public override void OnActionReceived(ActionBuffers bufferactions)
+    {
+        
+        Debug.Log(bufferactions.ContinuousActions[0]);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         }
+}
